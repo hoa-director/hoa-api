f8704911663c69dbb37fdb0aafbec53d88842590
John Gellert <gellertjm@gmail.com>
2020-05-26 23:15:43 -0500

    update database config

--- config/database.ts
+++ config/database.ts
@@ -1,52 +1,52 @@
+require('dotenv').config();
 import { Sequelize, Options, OperatorsAliases } from "sequelize";
 
-const logging = process.env.NODE_ENV === "development" ? console.log : false;
+const logging = process.env.NODE_ENV === "development" ? true : false;
 
-// const connectionOptions: Options = {
-//   host: process.env.DATABASE_HOST,
-//   dialect: 'mysql',
-//   pool: {
-//     max: 10,
-//     min: 0,
-//   },
-//   define: {
-//     underscored: true,
-//     paranoid: true,
-//   },
-//   logging,
-//   operatorsAliases: false,
-// };
-
-// class DatabaseConnection {
-//   sequelize: Sequelize;
-
-//   constructor() {
-//     this.sequelize = new Sequelize(
-//       process.env.DATABASE_DB,
-//       process.env.DATABASE_USER,
-//       process.env.DATABASE_PASSWORD,
-//       connectionOptions,
-//     );
-//   }
+const connectionOptions: Options = {
+  host: process.env.DATABASE_HOST,
+  dialect: 'mysql',
+  pool: {
+    max: 10,
+    min: 0,
+  },
+  define: {
+    underscored: true,
+    paranoid: true,
+  },
+  logging,
+  operatorsAliases: false,
+};
 
 class DatabaseConnection {
   sequelize: Sequelize;
 
   constructor() {
-    this.sequelize = new Sequelize(process.env.JAWSDB_URL);
+    if (process.env.JAWSDB_URL) {
+      this.sequelize = new Sequelize(process.env.JAWSDB_URL);
+    } else {
+      this.sequelize = new Sequelize(
+        process.env.DATABASE_DB,
+        process.env.DATABASE_USER,
+        process.env.DATABASE_PASSWORD,
+        connectionOptions
+      );
+    }
+    this.testConnection();
   }
 
   testConnection() {
     this.sequelize
       .authenticate()
       .then(() => {
-        console.log("connected to database");
+        console.log("Connected to database");
       })
       .catch((error) => {
         console.log("There was an error connecting to the database");
         console.error(error);
       });
   }
+  
 }
 
 export const connection = new DatabaseConnection().sequelize;
