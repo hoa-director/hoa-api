a9e149fe253f5c4a4ec5050881fcf089fa6b62da
John Gellert <gellertjm@gmail.com>
2021-12-07 03:52:28 -0600

    Update emailer

--- factories/emailer-factory.ts
+++ factories/emailer-factory.ts
@@ -12,9 +12,10 @@ export class EmailerFactory {
 
   static getTransporter(): Transporter {
     return createTransport({
+      pool: true,
       host: process.env.SMTP_SERVER,
       port: parseInt(process.env.SMTP_PORT, 10),
-      secure: true, // upgrade later with STARTTLS
+      secure: true, // use TLS
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASSWORD,
